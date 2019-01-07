60a2ce02619411b8dfc17209438444316f886a6c
WebManiaK <webmaniak@protonmail.ch>
2019-01-07 09:50:43 +0100

    Added an example of the make_list function + added some comments

--- functions_use.py
+++ functions_use.py
@@ -1,14 +1,40 @@
+# ------------------------------
+# Importing the "functions" module
+# and using it the in a raw way
+# ------------------------------
+
 #import functions
 #functions.greet("Alain")
+
+# ------------------------------
+# Importing the "functions" module
+# with a custom name (i.e. "f")
+# ------------------------------
 
 #import functions as f
 #f.greet("Alain")
 
+# ------------------------------
+# Importing part of the "functions" module
+# and using only what's been imported
+# ------------------------------
+
 #from functions import greet, greet_kids
 #greet("Alain")
+
+# ------------------------------
+# Importing only one function of the "functions" module
+# and giving it an alias
+# ------------------------------
 
 #from functions import greet_kids as hey
 #hey("Alain")
 
+# ------------------------------
+# Importing everything from the "functions" module
+# and using it without aliases
+# ------------------------------
+
 from functions import *
 greet_kids("Josh")
+print(make_list('list', 'array', 'collection', 'iterable'))
