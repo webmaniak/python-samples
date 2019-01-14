e368d1825d7a0394e5938343bf62df268236020c
WebManiaK <webmaniak@protonmail.ch>
2019-01-14 11:08:05 +0100

    Refactored code in order to select the snippet to execute in a simpler fashion

--- functions_use.py
+++ functions_use.py
@@ -7,47 +7,57 @@ A few use cases for functions defined in functions.py. (uncomment to execute)
 # and using it the in a raw way
 # ------------------------------
 
-#import functions
-#functions.greet("Alain")
+"""
+import functions
+functions.greet("Alain")
+"""
 
 # ------------------------------
 # Importing the "functions" module
 # with a custom name (i.e. "f")
 # ------------------------------
 
-#import functions as f
-#f.greet("Alain")
+"""
+import functions as f
+f.greet("Alain")
+"""
 
 # ------------------------------
 # Importing part of the "functions" module
 # and using only what's been imported
 # ------------------------------
 
-#from functions import greet, greet_kids
-#greet("Alain")
-#greet(name='Jeff') # will also work
+"""
+from functions import greet, greet_kids
+greet("Alain")
+greet(name='Jeff') # will also work
 
-#name = input("Please tell me your name: ")
-#greet_kids(uppercase_first_letter(name))
+name = input("Please tell me your name: ")
+greet_kids(uppercase_first_letter(name))
+"""
 
 # ------------------------------
 # Importing only one function of the "functions" module
 # and giving it an alias
 # ------------------------------
 
-#from functions import greet_kids as hey
-#hey("Alain")
+"""
+from functions import greet_kids as hey
+hey("Alain")
+"""
 
 # ------------------------------
 # Importing everything from the "functions" module
 # and using it without aliases
 # ------------------------------
 
-#from functions import *
+"""
+from functions import *
 
-#greet_kids("Josh")
-#print(make_list('list', 'array', 'collection', 'iterable'))
-#make_pizza(10, 'tomato sauce', 'mozarella', 'ham', 'mushrooms')
+greet_kids("Josh")
+print(make_list('list', 'array', 'collection', 'iterable'))
+make_pizza(10, 'tomato sauce', 'mozarella', 'ham', 'mushrooms')
+"""
 
 # ------------------------------
 # Demonstrating how functions ARE objects.
@@ -97,8 +107,12 @@ print(f'Multiply 5 by 3: {multiplier(5)}')
 # Use a class (almost) like a function
 # ------------------------------
 
+"""
 from classes.callable import Divider
 
 divide_by_2 = Divider(2)
 print(f'Is the Divider class a callable? Answer: {callable(divide_by_2)}')
 print(f'4 divided by 2 = {divide_by_2(4)}')
+
+"""
+
