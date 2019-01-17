028e2b0b9affad51cbadc29f3bc8946cef597f4e
WebManiaK <webmaniak@protonmail.ch>
2019-01-17 10:48:32 +0100

    Implemented str and repr methods on animals + Updated unit test

--- classes/animals.py
+++ classes/animals.py
@@ -9,15 +9,25 @@ class Animal():
         print("WTF?!?")
     
     def present(self):
-        print("This is an animal called " + self.name + " aged of "
-        + str(self.age) + " years.")
+        print(str(self))
+
+    def __str__(self):
+        return f'This is an animal called {self.name}' \
+                f' aged of {str(self.age)} years.'
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.name!r}, {self.age!r})'
 
 
 class Dog(Animal):
     """A simple representation of a dog."""
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, breed):
         super().__init__(name, age)
+        self.breed = breed
 
     def make_noise(self):
         print("Bark! Bark!")
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.name!r}, {self.age!r}, {self.breed!r})'
--- tests/dog_test.py
+++ tests/dog_test.py
@@ -5,13 +5,26 @@ class DogTestClass(unittest.TestCase):
     """Tests for animals.py"""
 
     def setUp(self):
-        self.dog = Dog("Barry", 5)
+        self.dog = Dog("Barry", 5, 'Golden Retriever')
 
     def test_dog_name(self):
         self.assertEqual(self.dog.name, "Barry")
 
     def test_dog_age(self):
         self.assertEqual(self.dog.age, 5)
+
+    def test_dog_breed(self):
+        self.assertEqual(self.dog.breed, 'Golden Retriever')
+
+    def test_dog_str(self):
+        expected_result = f'This is an animal called {self.dog.name}' \
+                f' aged of {str(self.dog.age)} years.'
+        self.assertEqual(str(self.dog), expected_result)
+
+    def test_dog_repr(self):
+        expected_result = f'Dog({self.dog.name!r}, {self.dog.age!r}, ' \
+                            f'{self.dog.breed!r})'
+        self.assertEqual(repr(self.dog), expected_result)
 
 if __name__ == '__main__':
     unittest.main()
